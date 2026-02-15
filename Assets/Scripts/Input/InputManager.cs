94535e5b8b3bc3874e3b4933bf0745228bc302c6
Yarrick87 <59537740+Yarrick87@users.noreply.github.com>
2026-02-16 00:48:44 +0100

    Add input for ios and android

--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -7,11 +7,19 @@ namespace DoubleTactics.Input
     {
         public void Update()
         {
+#if !UNITY_EDITOR && (UNITY_ANDROID || UNITY_IOS)
+            if (UnityEngine.Input.touchCount > 0)
+            {
+                var data = new InputClickEventData(UnityEngine.Input.GetTouch(0).position);
+                EventBus.Invoke(EventTypes.InputClick, data);
+            }
+#else
             if (UnityEngine.Input.GetMouseButtonDown(0))
             {
                 var data = new InputClickEventData(UnityEngine.Input.mousePosition);
                 EventBus.Invoke(EventTypes.InputClick, data);
             }
+#endif
         }
     }
 }
